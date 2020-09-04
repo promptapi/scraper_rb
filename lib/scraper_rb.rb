6666d1070f6a61a9978abbd3a0b1b3d3815bf4eb
Uğur Özyılmazel <ugur@promptapi.com>
2020-09-04 08:44:49 +0300

    Add basic tests

--- lib/scraper_rb.rb
+++ lib/scraper_rb.rb
@@ -1,6 +1,45 @@
 require "scraper_rb/version"
+require 'faraday'
+require 'json'
 
 module ScraperRb
+  @@data = nil
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.parse(body)
+    begin
+      response = JSON.parse(body, symbolize_names: true)
+    rescue JSON::ParserError
+      return {error: "JSON decoding error"}
+    end
+    response
+  end
+
+  def self.scrape(url:, params: nil, timeout: 10)
+    apikey = ENV['PROMPTAPI_TOKEN'] || nil
+    return {error: "You need to set PROMPTAPI_TOKEN environment variable"} unless apikey
+
+    promptapi_endpoint = ENV['PROMPTAPI_TEST_ENDPOINT'] || "https://api.promptapi.com/scraper"
+    conn = Faraday.new(url: promptapi_endpoint, request: {timeout: timeout}) do |c|
+      c.use Faraday::Response::RaiseError
+    end
+
+    begin
+      resp = conn.send(:get) do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['apikey'] = apikey
+      end
+    rescue Faraday::ConnectionFailed
+      return {error: "Connection error"}
+    rescue Faraday::TimeoutError => e
+      return {error: e.message.capitalize}
+    rescue Faraday::ClientError => e
+      return ::ScraperRb.parse(e.response[:body])
+    rescue Faraday::ServerError => e
+      return {error: e.message.capitalize}
+    end
+    
+    ::ScraperRb.parse(resp.body)
+  end
+
 end
--- test/scraper_rb_test.rb
+++ test/scraper_rb_test.rb
@@ -4,4 +4,42 @@ class ScraperRbTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ScraperRb::VERSION
   end
+
+  def test_environment_variable
+    buffer_token = nil
+    if ENV['PROMPTAPI_TOKEN']
+      buffer_token = ENV['PROMPTAPI_TOKEN']
+      ENV.delete('PROMPTAPI_TOKEN')
+    end
+
+    result = ScraperRb.scrape(url:"https://example.com")
+    refute_nil result.fetch(:error)
+    assert_equal result[:error], "You need to set PROMPTAPI_TOKEN environment variable"
+    ENV['PROMPTAPI_TOKEN'] = buffer_token if buffer_token
+  end
+
+  def test_connection_err
+    buffer_token = nil
+    buffer_endpoint = nil
+
+    if ENV['PROMPTAPI_TOKEN']
+      buffer_token = ENV['PROMPTAPI_TOKEN']
+      ENV.delete('PROMPTAPI_TOKEN')
+    end
+    if ENV['PROMPTAPI_TEST_ENDPOINT']
+      buffer_endpoint = ENV['PROMPTAPI_TEST_ENDPOINT']
+      ENV.delete('PROMPTAPI_TEST_ENDPOINT')
+    end
+
+    ENV['PROMPTAPI_TEST_ENDPOINT'] = "http://no-url-exists.com/"
+    ENV['PROMPTAPI_TOKEN'] = "123"
+
+    result = ScraperRb.scrape(url:"https://example.com")
+
+    refute_nil result.fetch(:error)
+    assert_equal result[:error], "Connection error"
+    
+    ENV['PROMPTAPI_TOKEN'] = buffer_token if buffer_token
+    ENV['PROMPTAPI_TEST_ENDPOINT'] = buffer_endpoint if buffer_endpoint
+  end
 end
